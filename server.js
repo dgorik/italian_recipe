e6989f538c1e41a2b6492063fd59db3e0cec3ad3
Daniel Gorbachev <dg186533@gmail.com>
2024-02-13 07:49:38 -0500

    working rapper api

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const rappers = {
+    '21 savage': {
+        'age': 29,
+        'birthName': 'Sheyea Bin Abrahan-Joseph',
+        'birthLocation': 'London, England'
+    },
+
+    'chance the rapper': {
+        'age': 29,
+        'birthName': 'Sheyea Bin Abrahan-Joseph',
+        'birthLocation': 'London, England'
+    },
+
+    'unknown': {
+        'age': 0,
+        'birthName': 'unknown',
+        'birthLocation': 'unknown',
+    },
+
+
+}
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html',)
+})
+
+app.get('/api/:name', (request, response) => {
+    const rapperName = request.params.name.toLowerCase()
+    if (rappers[rapperName]) {
+        response.json(rappers[rapperName])
+    }
+    else {
+        response.json(rappers['unknown'])
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`The server is now running on port ${PORT}! Betta Go Catch It`)
+})
+
