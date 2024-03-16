7dc2d4293520129b31b5b111675a59773e54b963
Daniel Gorbachev <dg186533@gmail.com>
2024-03-16 08:40:06 -0400

    edited the link

--- public/script.js
+++ public/script.js
@@ -4,7 +4,7 @@ document.querySelector('button').addEventListener('click', apiRequest)
 async function apiRequest() {
     const flower = document.querySelector('input').value
     try {
-        const response = await fetch(`https://group-flower-pics.onrender.com/api/${flower}`)
+        const response = await fetch(`https://flower-description-api.onrender.com/api/${flower}`)
         const data = await response.json()
 
         console.log(data)
